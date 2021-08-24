429ac4d6c5e41d430f3cbd882806d2b5e903d82b
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2021-08-23 22:27:11 -0400

    set up structure of main components

---
+++ easy-bank/src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            About Us
+            Contact
+            Blog
+            Careers
+            Support
+            Privacy Policy
+
+            Request Invite
+
+            © Easybank. All Rights Reserved
+        </footer>
+    )
+}
+
+export default Footer;
--- easy-bank/src/pages/LandingPage.js
+++ easy-bank/src/pages/LandingPage.js
@@ -1,11 +1,19 @@
 import React from "react";
+import Articles from "../components/Articles";
+import Content from "../components/Content";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Hero from "../components/Hero";
 
 const LandingPage = () => {
     return (
-        <div className="wrapper">
+        <>
             <Header />
-        </div>
+            <Hero />
+            <Content />
+            <Articles />
+            <Footer />
+        </>
     )
 }
 
