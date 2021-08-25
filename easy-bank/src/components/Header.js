81ca085e6ab7be0ce45d2cd2bfc4397d5f42e2f8
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2021-08-25 19:44:00 -0400

    align header items

--- easy-bank/src/components/Header.js
+++ easy-bank/src/components/Header.js
@@ -5,17 +5,19 @@ const Header = () => {
     return (
         <header>
             <div className="wrapper">
-                <div className="logo"><img src={logo} alt="Easy Bank" /></div>
-                <div className="nav-wrapper">
-                    <ul>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Contact</a></li>
-                        <li><a href="#">Blog</a></li>
-                        <li><a href="#">Careers</a></li>
-                    </ul>
+                <div className="flex header-container">
+                    <div className="logo"><img src={logo} alt="Easy Bank" /></div>
+                    <div className="nav-wrapper">
+                        <ul className="navigation">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Contact</a></li>
+                            <li><a href="#">Blog</a></li>
+                            <li><a href="#">Careers</a></li>
+                        </ul>
+                    </div>
+                    <div><button className="cta button">Request Invite</button></div>
                 </div>
-                <div><button className="cta button">Request Invite</button></div>
             </div>
         </header>
     )
