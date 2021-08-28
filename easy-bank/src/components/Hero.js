8758e43dd15033afd3b0b344309a8c93a0059d42
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2021-08-28 15:45:11 -0400

    start intro component

--- easy-bank/src/components/Hero.js
+++ easy-bank/src/components/Hero.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import introDesktop from "../images/bg-intro-desktop.svg";
 
 const Hero = () => {
     return (
         <section>
-            <h1>Next generation digital banking</h1>
-            <p>Take your financial life online. Your Easybank account will be a one-stop-shop  for spending, saving, budgeting, investing, and much more.</p>
-            <div><button className="cta button">Request Invite</button></div>
+            <div className="hero">
+                <div className="wrapper">
+                    <div className="intro-message">
+                        <h1>Next generation digital banking</h1>
+                        <p>Take your financial life online. Your Easybank account will be a one-stop-shop  for spending, saving, budgeting, investing, and much more.</p>
+                        <div><button className="cta button">Request Invite</button></div>
+                    </div>
+                    <div className="intro-image-wrapper">
+                        <div className="intro-image"></div>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
