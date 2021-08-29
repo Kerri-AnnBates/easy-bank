d10c5b82e8aea657c2988c714cc3559dfd4903d3
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2021-08-29 13:55:23 -0400

    intro bg image placement

--- easy-bank/src/components/Hero.js
+++ easy-bank/src/components/Hero.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import introDesktop from "../images/bg-intro-desktop.svg";
+import introPhone from "../images/image-mockups.png";
 
 const Hero = () => {
     return (
         <section>
             <div className="hero">
                 <div className="wrapper">
-                    <div className="intro-message">
-                        <h1>Next generation digital banking</h1>
-                        <p>Take your financial life online. Your Easybank account will be a one-stop-shop  for spending, saving, budgeting, investing, and much more.</p>
-                        <div><button className="cta button">Request Invite</button></div>
+                    <div className="intro">
+                        <div className="intro-message">
+                            <h1>Next generation digital banking</h1>
+                            <p>Take your financial life online. Your Easybank account will be a one-stop-shop  for spending, saving, budgeting, investing, and much more.</p>
+                            <div><button className="cta button">Request Invite</button></div>
+                        </div>
                     </div>
-                    <div className="intro-image-wrapper">
-                        <div className="intro-image"></div>
-                    </div>
+                </div>
+                <div className="bg-wrapper">
+                    <div className="bg-intro"></div>
                 </div>
             </div>
         </section>
