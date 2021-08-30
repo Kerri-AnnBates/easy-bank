0a1611eb02fda320336945aa17234288e03db8ce
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2021-08-30 12:33:41 -0400

    start styles for content section

--- easy-bank/src/components/Hero.js
+++ easy-bank/src/components/Hero.js
@@ -4,23 +4,21 @@ import introPhone from "../images/image-mockups.png";
 
 const Hero = () => {
     return (
-        <section>
-            <div className="hero">
-                <div className="wrapper">
-                    <div className="intro">
-                        <div className="intro-message">
-                            <h1>Next generation digital banking</h1>
-                            <p>Take your financial life online. Your Easybank account will be a one-stop-shop  for spending, saving, budgeting, investing, and much more.</p>
-                            <div><button className="cta button">Request Invite</button></div>
-                        </div>
-                        <div className="img-wrapper">
-                            <img src={introPhone} />
-                        </div>
+        <section className="hero">
+            <div className="wrapper">
+                <div className="intro">
+                    <div className="intro-message">
+                        <h1>Next generation digital banking</h1>
+                        <p>Take your financial life online. Your Easybank account will be a one-stop-shop  for spending, saving, budgeting, investing, and much more.</p>
+                        <div><button className="cta button">Request Invite</button></div>
+                    </div>
+                    <div className="img-wrapper">
+                        <img src={introPhone} />
                     </div>
                 </div>
-                <div className="bg-wrapper">
-                    <div className="bg-intro"></div>
-                </div>
+            </div>
+            <div className="bg-wrapper">
+                <div className="bg-intro"></div>
             </div>
         </section>
     )
