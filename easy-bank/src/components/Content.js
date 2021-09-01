2342f34404e0fcf5d106d929d870e38f491d3aef
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2021-09-01 19:06:13 -0400

    align and style feature blocks

--- easy-bank/src/components/Content.js
+++ easy-bank/src/components/Content.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import apiIcon from "../images/icon-api.svg";
+import budgetIcon from "../images/icon-budgeting.svg";
+import onlineIcon from "../images/icon-online.svg";
+import onboardingIcon from "../images/icon-onboarding.svg";
 
 const Content = () => {
     return (
@@ -9,21 +13,33 @@ const Content = () => {
                     <p>We leverage Open Banking to turn your bank account into your financial hub. Control your finances like never before.</p>
                 </div>
 
-                <div className="content_feature">
+                <div className="content_feature flex">
                     <div className="feature-block">
-                        <h2>Online Banking</h2>
+                        <div className="icon-container">
+                            <img src={onlineIcon} alt="Online Banking Icon" />
+                        </div>
+                        <h3>Online Banking</h3>
                         <p>Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world.</p>
                     </div>
                     <div className="feature-block">
-                        <h2>Simple Budgeting</h2>
+                        <div className="icon-container">
+                            <img src={budgetIcon} alt="Budget Icon" />
+                        </div>
+                        <h3>Simple Budgeting</h3>
                         <p>See exactly where your money goes each month. Receive notifications when you’re close to hitting your limits.</p>
                     </div>
                     <div className="feature-block">
-                        <h2>Fast Onboarding</h2>
+                        <div className="icon-container">
+                            <img src={onboardingIcon} alt="Onboarding Icon" />
+                        </div>
+                        <h3>Fast Onboarding</h3>
                         <p>We don’t do branches. Open your account in minutes online and start taking control of your finances right away.</p>
                     </div>
                     <div className="feature-block">
-                        <h2>Open API</h2>
+                        <div className="icon-container">
+                            <img src={apiIcon} alt="API Icon" />
+                        </div>
+                        <h3>Open API</h3>
                         <p>Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier.</p>
                     </div>
                 </div>
