61db3560d0901219ff9718729952d9a6c93da550
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2021-08-31 22:11:45 -0400

    set up content structure

--- easy-bank/src/components/Content.js
+++ easy-bank/src/components/Content.js
@@ -4,20 +4,29 @@ const Content = () => {
     return (
         <section className="content">
             <div className="wrapper">
-                <h2>Why choose Easybank?</h2>
-                <p>We leverage Open Banking to turn your bank account into your financial hub. Control your finances like never before.</p>
+                <div className="content_header">
+                    <h2>Why choose Easybank?</h2>
+                    <p>We leverage Open Banking to turn your bank account into your financial hub. Control your finances like never before.</p>
+                </div>
 
-                <h2>Online Banking</h2>
-                <p>Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world.</p>
-
-                <h2>Simple Budgeting</h2>
-                <p>See exactly where your money goes each month. Receive notifications when you’re close to hitting your limits.</p>
-
-                <h2>Fast Onboarding</h2>
-                <p>We don’t do branches. Open your account in minutes online and start taking control of your finances right away.</p>
-
-                <h2>Open API</h2>
-                <p>Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier.</p>
+                <div className="content_feature">
+                    <div className="feature-block">
+                        <h2>Online Banking</h2>
+                        <p>Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world.</p>
+                    </div>
+                    <div className="feature-block">
+                        <h2>Simple Budgeting</h2>
+                        <p>See exactly where your money goes each month. Receive notifications when you’re close to hitting your limits.</p>
+                    </div>
+                    <div className="feature-block">
+                        <h2>Fast Onboarding</h2>
+                        <p>We don’t do branches. Open your account in minutes online and start taking control of your finances right away.</p>
+                    </div>
+                    <div className="feature-block">
+                        <h2>Open API</h2>
+                        <p>Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier.</p>
+                    </div>
+                </div>
             </div>
         </section>
     )
