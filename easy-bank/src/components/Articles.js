8003abb9692843cf843fac4bef8cf7caf4c6ae34
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2021-09-08 21:36:34 -0400

    style articles

--- easy-bank/src/components/Articles.js
+++ easy-bank/src/components/Articles.js
@@ -6,7 +6,7 @@ import imageRestaurant from "../images/image-restaurant.jpg";
 
 const Articles = () => {
     return (
-        <section>
+        <section className="articles">
             <div className="wrapper">
                 <h2>Latest Articles</h2>
                 <div className="article_content flex">
@@ -15,9 +15,9 @@ const Articles = () => {
                             <img src={imageCurrency} alt="Stacks of currency" />
                         </div>
                         <div className="article_excerpt">
-                            <p>By Claire Robinson</p>
-                            <p>Receive money in any currency with no fees
-                                The world is getting smaller and we’re becoming more mobile. So why should you be
+                            <p className="sm-text author">By Claire Robinson</p>
+                            <h3 className="sm-heading">Receive money in any currency with no fees</h3>
+                            <p>The world is getting smaller and we’re becoming more mobile. So why should you be
                                 forced to only receive money in a single …</p>
                         </div>
                     </div>
@@ -26,9 +26,9 @@ const Articles = () => {
                             <img src={imageRestaurant} alt="Dinner table at a restaurant" />
                         </div>
                         <div className="article_excerpt">
-                            <p>By Wilson Hutton</p>
-                            <p>Treat yourself without worrying about money
-                                Our simple budgeting feature allows you to separate out your spending and set
+                            <p className="sm-text author">By Wilson Hutton</p>
+                            <h3 className="sm-heading">Treat yourself without worrying about money</h3>
+                            <p>Our simple budgeting feature allows you to separate out your spending and set
                                 realistic limits each month. That means you …</p>
                         </div>
                     </div>
@@ -37,9 +37,9 @@ const Articles = () => {
                             <img src={imagePlane} alt="Plane wing" />
                         </div>
                         <div className="article_excerpt">
-                            <p>By Wilson Hutton</p>
-                            <p>Take your Easybank card wherever you go
-                                We want you to enjoy your travels. This is why we don’t charge any fees on purchases
+                            <p className="sm-text author">By Wilson Hutton</p>
+                            <h3 className="sm-heading">Take your Easybank card wherever you go</h3>
+                            <p>We want you to enjoy your travels. This is why we don’t charge any fees on purchases
                                 while you’re abroad. We’ll even show you …</p>
                         </div>
                     </div>
@@ -48,9 +48,9 @@ const Articles = () => {
                             <img src={imageConfettit} alt="Floating confetti" />
                         </div>
                         <div className="article_excerpt">
-                            <p>By Claire Robinson</p>
-                            <p>Our invite-only Beta accounts are now live!
-                                After a lot of hard work by the whole team, we’re excited to launch our closed beta.
+                            <p className="sm-text author">By Claire Robinson</p>
+                            <h3 className="sm-heading">Our invite-only Beta accounts are now live!</h3>
+                            <p>After a lot of hard work by the whole team, we’re excited to launch our closed beta.
                                 It’s easy to request an invite through the site ...</p>
                         </div>
                     </div>
