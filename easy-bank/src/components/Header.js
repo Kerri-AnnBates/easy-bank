4f84eb4eb139a862b04e61bfe986fe37cd39a55e
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2021-12-06 22:04:08 -0500

    base mobile styles for header navigation

--- easy-bank/src/components/Header.js
+++ easy-bank/src/components/Header.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from "../images/logo.svg";
+import hamburger from "../images/icon-hamburger.svg";
+import closeIcon from "../images/icon-close.svg";
 
 const Header = () => {
+    const [isOpen, setisOpen] = useState(false);
+
+    const handleMenu = () => {
+        setisOpen(!isOpen);
+    }
+
     return (
         <header>
             <div className="wrapper">
                 <div className="flex header-container">
                     <div className="logo"><img src={logo} alt="Easy Bank" /></div>
-                    <div className="nav-wrapper">
+                    <div className="mobile-nav"><button className="menu-button" onClick={() => handleMenu()}><img src={isOpen ? closeIcon : hamburger} alt="hamburger" /></button></div>
+                    <div className={`nav-wrapper ${isOpen ? "open" : ""}`}>
                         <ul className="navigation">
                             <li><a href="#">Home</a></li>
                             <li><a href="#">About</a></li>
@@ -16,7 +25,7 @@ const Header = () => {
                             <li><a href="#">Careers</a></li>
                         </ul>
                     </div>
-                    <div><button className="cta button">Request Invite</button></div>
+                    <div className="header-action"><button className="cta button">Request Invite</button></div>
                 </div>
             </div>
         </header>
